e34601fe306350d03a0e8d78d0801080b9a5c9e7
milandeep singh bhalla <milandeepbhalla@outlook.com>
2025-11-21 06:57:05 -0800

    add member api done

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,9 +1,11 @@
-const express = require("express")
-const { validateToken, adminCheck } = require("../middlewares/auth")
-const Team = require("../models/Team")
-const { createTeamRules, validateRules } = require("../middlewares/validators")
+const express = require("express");
+const { validateToken, adminCheck } = require("../middlewares/auth");
+const Team = require("../models/Team");
+const { createTeamRules, validateRules } = require("../middlewares/validators");
+const User = require("../models/User");
+const { sendEmail, generateTemporaryPassword } = require("../util");
 
-const adminRouter = express.Router()
+const adminRouter = express.Router();
 
 adminRouter.post(
   "/create-team",
@@ -13,39 +15,39 @@ adminRouter.post(
   validateRules,
   async (req, res) => {
     try {
-      let { teamName, description = "" } = req.body
-      let existingTeam = await Team.find({ teamName: teamName })
+      let { teamName, description = "" } = req.body;
+      let existingTeam = await Team.find({ teamName: teamName });
       if (existingTeam.length > 0) {
         return res.status(401).json({
           sucess: false,
           errors: {
             teamName: `Team with name ${teamName} already exist.`,
           },
-        })
+        });
       }
       let newTeamDoc = new Team({
         teamName,
         description,
         companyId: req.company._id,
-      })
-      let newTeam = await newTeamDoc.save()
+      });
+      let newTeam = await newTeamDoc.save();
       return res.status(201).json({
         success: true,
         message: "Team created.",
-      })
+      });
     } catch (e) {
-      console.log("Error while creating team.", e)
+      console.log("Error while creating team.", e);
       return res.status(401).json({
         sucess: false,
         errors: {
           error: e.message || "Error while creating team.",
         },
-      })
+      });
     }
   }
-)
+);
 
-adminRouter.get("/get-teams",validateToken,adminCheck,async (req,res)=>{
+adminRouter.get("/get-teams", validateToken, adminCheck, async (req, res) => {
   try {
     let page = parseInt(req.query.page) || 1;
     let limit = parseInt(req.query.limit) || 5;
@@ -53,37 +55,166 @@ adminRouter.get("/get-teams",validateToken,adminCheck,async (req,res)=>{
     if (page < 1) page = 1;
     if (limit > 100) limit = 100;
 
-    console.log("company id:-",req.company._id)
+    console.log("company id:-", req.company._id);
 
-    let totalTeams = await Team.countDocuments({companyId: req.company._id, isDeleted: false})
-    let noOfPages = Math.ceil((totalTeams/limit))
+    let totalTeams = await Team.countDocuments({
+      companyId: req.company._id,
+      isDeleted: false,
+    });
+    let noOfPages = Math.ceil(totalTeams / limit);
 
-    let skip = (page-1)  * limit
+    let skip = (page - 1) * limit;
 
-    const foundTeams = await Team.find({companyId: req.company._id, isDeleted: false},{
-      isDeleted: 0,
-      __v: 0,
-      companyId: 0,
-      createdAt: 0,
-      updatedAt: 0
-    }).sort({createdAt: -1}).skip(skip).limit(limit).populate("managerId","fullName email")
+    const foundTeams = await Team.find(
+      { companyId: req.company._id, isDeleted: false },
+      {
+        isDeleted: 0,
+        __v: 0,
+        companyId: 0,
+        createdAt: 0,
+        updatedAt: 0,
+      }
+    )
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("managerId", "fullName email");
 
     return res.status(200).json({
       sucess: true,
       teams: foundTeams,
       totalTeams,
       noOfPages,
-    })
-
-
+    });
   } catch (err) {
     return res.status(400).json({
       success: false,
       errors: {
-        error: err.message || "Error while getting teams."
-      }
-    })
+        error: err.message || "Error while getting teams.",
+      },
+    });
   }
-})
+});
 
-module.exports = adminRouter
+adminRouter.post("/add-member", validateToken, adminCheck, async (req, res) => {
+  try {
+    const { fullName, email, teamId, teamName } = req.body;
+    const role = ["employee"];
+    let companyCheck = await User.findOne({
+      email: email,
+    });
+    if (companyCheck) {
+      if (companyCheck.companyId.toString() != req.company._id.toString()) {
+        return res.status(401).json({
+          success: false,
+          errors: {
+            email: `User already present in another company.`,
+          },
+        });
+      }
+    }
+    // check if user alread in same  team
+    let teamCheck = await User.findOne({
+      email: email,
+      teamId: teamId,
+    });
+    if (teamCheck) {
+      return res.status(400).json({
+        success: false,
+        errors: {
+          email: `${email} is already present in ${teamName}`,
+        },
+      });
+    }
+    let existingUser = await User.findOne({
+      email: email,
+      companyId: req.company._id,
+    }).populate("teamId", "teamName");
+    if (existingUser) {
+      // if already in another team get teamName and send back as error already existing in team
+      if (existingUser.teamId) {
+        return res.status(400).json({
+          success: false,
+          errors: {
+            email: `${email} is already present in ${existingUser.teamId.teamName}`,
+          },
+        });
+      }
+      // else add user to the team and send mail
+      existingUser.teamId = teamId;
+      await existingUser.save();
+      // find team and update size
+      let updatedTeam = await Team.findOneAndUpdate(
+        {
+          _id: teamId,
+        },
+        {
+          $inc: {
+            size: 1,
+          },
+        },
+        {
+          new: true,
+        }
+      );
+
+      await sendEmail(
+        email,
+        `Congratulations you have been added to team- ${teamName}.`,
+        "ExistingUser",
+        { fullName: fullName, teamName: teamName },
+        res
+      );
+    } else {
+      // generate a temporaryPassword
+      const tempPassword = generateTemporaryPassword();
+      // create new user
+      let user = new User({
+        fullName,
+        email,
+        role,
+        teamId,
+        companyId: req.company._id,
+        password: tempPassword,
+      });
+
+      await user.save();
+      let updatedTeam = await Team.findOneAndUpdate(
+        {
+          _id: teamId,
+        },
+        {
+          $inc: {
+            size: 1,
+          },
+        },
+        {
+          new: true,
+        }
+      );
+
+      await sendEmail(
+        email,
+        `Congratulations you have been added to team- ${teamName}.`,
+        "NewUser",
+        {
+          fullName: fullName,
+          teamName: teamName,
+          email: email,
+          tempPassword: tempPassword,
+        },
+        res
+      );
+    }
+  } catch (err) {
+    console.log("error while adding member", err);
+    return res.status(500).json({
+      success: false,
+      errors: {
+        error: err.message || "Error while addiing member.",
+      },
+    });
+  }
+});
+
+module.exports = adminRouter;
--- util.js
+++ util.js
@@ -1,19 +1,19 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 const hbs =
   require("nodemailer-express-handlebars").default ||
-  require("nodemailer-express-handlebars")
-const path = require("path")
-const dotenv = require("dotenv")
-dotenv.config()
+  require("nodemailer-express-handlebars");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
 const errorHandlerFunction = (msg, e, res) => {
-  console.log(msg, e)
+  console.log(msg, e);
   return res.status(401).json({
     success: false,
     errors: {
       error: e.message || msg,
     },
-  })
-}
+  });
+};
 
 // const transporter = nodemailer.createTransport({
 //   host: process.env.EMAIL_HOST, // 'smtp-mail.outlook.com'
@@ -25,9 +25,9 @@ const errorHandlerFunction = (msg, e, res) => {
 //   },
 // })
 
-const EMAIL_USER = process.env.EMAIL_USER
+const EMAIL_USER = process.env.EMAIL_USER;
 
-const APP_PASSWORD = process.env.EMAIL_PASS
+const APP_PASSWORD = process.env.EMAIL_PASS;
 
 // const transporter = nodemailer.createTransport({
 //   host: process.env.EMAIL_HOST, // or 'smtp-mail.outlook.com'
@@ -51,7 +51,7 @@ const transporter = nodemailer.createTransport({
     // The App Password is used as the 'pass'
     pass: APP_PASSWORD,
   },
-})
+});
 
 const hbsOptions = {
   viewEngine: {
@@ -60,9 +60,9 @@ const hbsOptions = {
   },
   viewPath: path.resolve("./views"),
   extName: ".hbs",
-}
+};
 
-transporter.use("compile", hbs(hbsOptions))
+transporter.use("compile", hbs(hbsOptions));
 
 const sendEmail = async (to, subject, template, context, res) => {
   try {
@@ -72,19 +72,74 @@ const sendEmail = async (to, subject, template, context, res) => {
       subject,
       template,
       context,
-    }
+    };
 
     // Send email options using the transporter
-    let info = await transporter.sendMail(mailOptions)
-    console.log("\n✉️ Message sent successfully!")
-    console.log("Message ID: %s", info.messageId)
+    let info = await transporter.sendMail(mailOptions);
+    console.log("\n✉️ Message sent successfully!");
+    console.log("Message ID: %s", info.messageId);
+    return res.status(200).json({
+      success: true,
+      message: "Member added",
+    });
   } catch (error) {
-    console.error("\n❌ FAILED TO SEND EMAIL:")
-    console.error(error.response || error)
+    console.error("\n❌ FAILED TO SEND EMAIL:");
+    console.error(error.response || error);
+    return res.status(500).json({
+      success: false,
+      errors: {
+        error: error.message || "Some error while adding user.",
+      },
+    });
   }
+};
+
+function generateTemporaryPassword(length = 12) {
+  // Define the character sets to use
+  const charset = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "@#$",
+  };
+
+  // Combine all character sets for general use
+  const allChars = Object.values(charset).join("");
+
+  let password = "";
+
+  // 1. Ensure the password contains at least one character from each set
+  // This makes the password meet common complexity requirements
+  const requiredChars = [
+    charset.lowercase,
+    charset.uppercase,
+    charset.numbers,
+    charset.symbols,
+  ];
+
+  for (const set of requiredChars) {
+    // Pick a random character from the current required set
+    const randomChar = set.charAt(Math.floor(Math.random() * set.length));
+    password += randomChar;
+  }
+
+  // 2. Fill the remaining length with random characters from the combined set
+  for (let i = password.length; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+  }
+
+  // 3. Shuffle the password to ensure the required characters are not just at the start
+  password = password
+    .split("")
+    .sort(() => 0.5 - Math.random())
+    .join("");
+
+  return password;
 }
 
 module.exports = {
   errorHandlerFunction,
   sendEmail,
-}
+  generateTemporaryPassword,
+};
